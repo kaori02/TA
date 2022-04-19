6a5214e0ce2bb0a2604bcef9b01c3dd2ecb31a29
kaori02 <kaorigaming02@gmail.com>
2022-04-19 16:53:56 +0700

    change address to 0x44

--- LIDARLite_RaspberryPi_Library/examples/llv3.cpp
+++ LIDARLite_RaspberryPi_Library/examples/llv3.cpp
@@ -23,34 +23,34 @@
 
 LIDARLite_v3 myLidarLite;
 
-int main()
-{
-    __u16 distance;
-    __u8  busyFlag;
+// int main()
+// {
+//     __u16 distance;
+//     __u8  busyFlag;
 
-    // Initialize i2c peripheral in the cpu core
-    myLidarLite.i2c_init();
+//     // Initialize i2c peripheral in the cpu core
+//     myLidarLite.i2c_init();
 
-    // Optionally configure LIDAR-Lite
-    myLidarLite.configure(0);
+//     // Optionally configure LIDAR-Lite
+//     myLidarLite.configure(0);
 
-    while(1)
-    {
-        // Each time through the loop, check BUSY
-        busyFlag = myLidarLite.getBusyFlag();
+//     while(1)
+//     {
+//         // Each time through the loop, check BUSY
+//         busyFlag = myLidarLite.getBusyFlag();
 
-        if (busyFlag == 0x00)
-        {
-            // When no longer busy, immediately initialize another measurement
-            // and then read the distance data from the last measurement.
-            // This method will result in faster I2C rep rates.
-            myLidarLite.takeRange();
-            distance = myLidarLite.readDistance();
+//         if (busyFlag == 0x00)
+//         {
+//             // When no longer busy, immediately initialize another measurement
+//             // and then read the distance data from the last measurement.
+//             // This method will result in faster I2C rep rates.
+//             myLidarLite.takeRange();
+//             distance = myLidarLite.readDistance();
 
-            printf("%4d\n", distance);
-        }
-    }
-}
+//             printf("%4d\n", distance);
+//         }
+//     }
+// }
 
 
 /*
@@ -61,7 +61,7 @@ int main()
  * ================================================================
  */
 
-/*
+
 #define i2cSecondaryAddr 0x44  // Set I2C address of LIDAR-Lite v3 to 0x44
 
 int main()
@@ -84,4 +84,4 @@ int main()
         printf("%4d\n", distance);
     }
 }
-*/
+
