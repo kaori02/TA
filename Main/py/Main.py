3a212764f82fc5281ae13239336608bc66a43b0b
kaori02 <kaorigaming02@gmail.com>
2022-06-02 09:45:46 +0700

    rapiin dikit

--- Main/py/Main.py
+++ Main/py/Main.py
@@ -3,14 +3,18 @@
 
 from subprocess import Popen, PIPE
 
-program_path = "./bin/0x44_llv3.out & ./bin/0x62_llv3.out"
+try:
+  while True:
+    lidar_0x44 = Popen(["./bin/0x44_llv3.out"], stdout=PIPE)
+    lidar_0x62 = Popen(["./bin/0x62_llv3.out"], stdout=PIPE)
 
-while(1):
-  p = Popen(["./bin/0x44_llv3.out"], stdout=PIPE)
-  o = Popen(["./bin/0x62_llv3.out"], stdout=PIPE)
+    str_0x44 = lidar_0x44.stdout.readline().decode("utf-8").strip()
+    str_0x62 = lidar_0x62.stdout.readline().decode("utf-8").strip()
 
-  result1 = p.stdout.readline().decode("utf-8").strip()
-  result2 = o.stdout.readline().decode("utf-8").strip()
-
-  print(result1)
-  print(result2)
+    print("[0x44]\t"+str_0x44)
+    print("[0x62]\t"+str_0x62)
+except KeyboardInterrupt:
+  lidar_0x44.kill()
+  lidar_0x62.kill()
+  print("interrupt")
+  sys.exit
