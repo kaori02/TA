2dacb0941a7899a33c4676806ca61c63aee8e786
kaori02 <kaorigaming02@gmail.com>
2022-06-02 12:48:04 +0700

    create separated class for lidar reader

---
+++ Main/py/LidarReader.py
@@ -0,0 +1,18 @@
+from subprocess import Popen, PIPE
+
+class LidarReader:
+  # lidar_proc = None
+  def __init__(self, path):
+    self.path = path
+    self.name = path.replace('_llv3.out', '').replace('./bin/', '')
+
+  def readData(self):
+    self.lidar_proc = Popen([self.path], stdout=PIPE)
+    return self.lidar_proc.stdout.readline().decode("utf-8").strip()
+
+  def readName(self):
+    return self.name
+
+  def __del__(self):
+    if hasattr(self, 'self.lidar_proc'):
+      self.lidar_proc.kill()
--- Main/py/Main.py
+++ Main/py/Main.py
@@ -1,20 +1,17 @@
 # Skrip untuk ambil data LiDAR, logika, dan kirim perintah ke drone
 # - Ambil data dari kedua LiDAR sekaligus algonya pake script py
 
-from subprocess import Popen, PIPE
+from LidarReader import LidarReader
+import sys
 
 try:
+  lidar_0x44 = LidarReader("./bin/0x44_llv3.out")
+  lidar_0x62 = LidarReader("./bin/0x62_llv3.out")
   while True:
-    lidar_0x44 = Popen(["./bin/0x44_llv3.out"], stdout=PIPE)
-    lidar_0x62 = Popen(["./bin/0x62_llv3.out"], stdout=PIPE)
-
-    str_0x44 = lidar_0x44.stdout.readline().decode("utf-8").strip()
-    str_0x62 = lidar_0x62.stdout.readline().decode("utf-8").strip()
-
-    print("[0x44]\t"+str_0x44)
-    print("[0x62]\t"+str_0x62)
+    print(lidar_0x44.readName()+"\t"+lidar_0x44.readData())
+    print(lidar_0x62.readName()+"\t"+lidar_0x62.readData())
 except KeyboardInterrupt:
-  lidar_0x44.kill()
-  lidar_0x62.kill()
+  del lidar_0x44
+  del lidar_0x62
   print("interrupt")
   sys.exit
