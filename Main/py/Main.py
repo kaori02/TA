dceec54d981f9513967c8b93ee10068e4ed817b6
kaori02 <kaorigaming02@gmail.com>
2022-06-03 09:59:32 +0700

    tambahin kode lama dlu

--- Main/py/Main.py
+++ Main/py/Main.py
@@ -115,4 +115,15 @@ def main():
       sys.exit
 
 if __name__ == "__main__":
-  main()
+  # main()
+  try:
+    lidar_0x44 = LidarReader("./bin/0x44_llv3.out")
+    lidar_0x62 = LidarReader("./bin/0x62_llv3.out")
+    while True:
+      print(lidar_0x44.readName()+"\t"+lidar_0x44.readData())
+      print(lidar_0x62.readName()+"\t"+lidar_0x62.readData())
+  except KeyboardInterrupt:
+    del lidar_0x44
+    del lidar_0x62
+    print("interrupt")
+    sys.exit
